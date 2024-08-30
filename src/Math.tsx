41dcf6c01450eed47c96a99496646205861072f7
jspaniac <jspaniac@cs.washington.edu>
2024-08-29 21:10:46 -0700

    Refactoring Vector

---
+++ src/Math.tsx
@@ -0,0 +1,35 @@
+export class Vector {
+    private vals: number[];
+
+    constructor(dims: number) {
+        this.vals = new Array(dims);
+    }
+
+    set(i: number, val: number) {
+        if (i < 0 || i >= this.vals.length) {
+            throw new Error("Invalid provided index");
+        }
+        this.vals[i] = val;
+    }
+
+    get(i: number) {
+        if (i < 0 || i >= this.vals.length) {
+            throw new Error("Invalid provided index");
+        }
+        return this.vals[i];
+    }
+
+    sum(): number {
+        return this.vals.reduce((acc: number, val: number) => {
+            return acc + val;
+        }, 0);
+    }
+
+    static create(vals: number[]) {
+        const ret: Vector = new Vector(vals.length)
+        vals.forEach((val: number, index: number) => {
+            ret.vals[index] = val;
+        })
+        return ret;
+    }
+}
